a8d299cc2cd45bb47a6aa7d24bf4a7bb688c39fa
chakal <54026816+chakal1337@users.noreply.github.com>
2022-11-28 12:24:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo apt install apache2 php php-mysql mariadb-server python3 python3-pip;
 sudo service mysql start;
-sudo mysql -u root <<< "alter user 'root'@'localhost' identified by 'root';";
+sudo mysql -u'root' <<< "alter user 'root'@'localhost' identified by 'root';";
+sudo mysql -u'root' -p'root' < db.sql;
 pip3 install requests mysql-connector bs4;
 sudo service apache2 restart;
 sudo cp www/* /var/www/html/;
